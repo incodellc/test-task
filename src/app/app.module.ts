b56e03ac8900dec3accbc69abd35c8e74a948980
Maksym Bormot <sigma2004y@gmail.com>
2018-11-30 15:57:24 +0200

    fixed pagination

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import {MaterialModule} from './material-module';
+import { FacilityComponent } from './components/facility.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FacilityComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
